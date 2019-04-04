dae6eaf541157f814b36d0ab2218a68e935c8615
venren <47970317+venren91@users.noreply.github.com>
2019-04-04 17:33:16 +0200

    Update Main.java

--- messaging/src/net/venren/messaging/Main.java
+++ messaging/src/net/venren/messaging/Main.java
@@ -8,7 +8,7 @@ import java.net.URISyntaxException;
 public class Main {
     public static void main(String[] args) throws Exception {
         AMQConsumer amqConsumer = new AMQConsumer(new URI("tcp://localhost:61616"), "app");
-        TopicSubscriber ts = amqConsumer.getTopicSubscriber("videoapi.Recording Started","firstconsumer");
+        TopicSubscriber ts = amqConsumer.getTopicSubscriber("*.sometopic","firstconsumer");
         MessageListener ml = new CustomMessageListener();
         ts.setMessageListener(ml);
         
